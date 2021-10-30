3e5d8f4e61e57fa62dc46263fc427eb0512cbb7e
whmsft <Whirlpool.programmer@outlook.com>
2021-10-30 15:46:34 +0530

    no more --onefile

--- freeze.sh
+++ freeze.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt
 pip install pyinstaller
-pyinstaller --noconfirm --onefile --windowed --icon "./DATA/icons/favicon.v3.ico" --name "WhirlEdit" --add-data "./data.py;." --add-data "./widgets.py;." --add-data "./tkcode;tkcode/"  "main.py"
+pyinstaller --noconfirm --windowed --icon "./DATA/icons/favicon.v3.ico" --name "WhirlEdit" --add-data "./data.py;." --add-data "./widgets.py;." --add-data "./tkcode;tkcode/"  "main.py"
