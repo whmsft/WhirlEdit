6ddd3362d9f1e12731a86796843b5df7d72a7f8b
whmsft <Whirlpool.programmer@outlook.com>
2021-11-16 15:10:17 +0530

    Looking for new method for freezing

--- freeze.sh
+++
@@ -1,3 +0,0 @@
-pip install -r requirements.txt
-pip install pyinstaller
-pyinstaller --noconfirm --windowed --icon "./DATA/icons/favicon.v3.ico" --name "WhirlEdit" --add-data "./data.py;." --add-data "./widgets.py;." --add-data "./tkcode;tkcode/"  "main.py"
