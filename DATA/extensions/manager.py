3fe2fe1506d0b5443e4772bb07a1b2383d77da11
whmsft <Whirlpool.programmer@outlook.com>
2021-11-23 21:02:54 +0530

    extmanager under dev

--- DATA/extensions/manager.py
+++ DATA/extensions/manager.py
@@ -14,7 +14,31 @@ class tasks:
         pass
     class after_extension_installation:
         pass
+    class main_vars_definition:
+        pass
+    class main_funcs_definition:
+        pass
+    class main_classes_definition:
+        pass
+    class before_root_definition:
+        pass
+    class after_root_definition:
+        pass
+    class sidebar_widgets:
+        pass
+    class before_scheme_definition:
+        pass
+    class before_mainloop:
+        pass
+    class before_configs_save:
+        pass
+    class onexit:
+        pass
 
 print('Extension Manager platform 1')
 print('...')
 
+for i in os.listdir('./'):
+    print(i)
+
+import time; time.sleep(10)
