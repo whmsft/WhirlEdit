c00220e243c47335ac690e25ca013bbb31c5ff3f
whmsft <whmsft@outlook.com>
2022-03-09 13:44:37 +0530

    lexers fix

--- DATA/Lexers/__init__.py
+++ DATA/Lexers/__init__.py
@@ -5,5 +5,5 @@ try:
 except:
     pass
 for i in os.listdir('./DATA/Lexers/'):
-    if i != '__init__.py' or i != '__pycache__':
+    if i not in '__init__.py__pycache__':
         exec('from . import {}'.format(i[:-3]))
