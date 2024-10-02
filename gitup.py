4ad9d53dcec12bb589c5f5597257f4658a69b002
whmsft <whmsft@outlook.com>
2024-10-02 16:45:53 +0530

    Delete gitup.py

--- gitup.py
+++
@@ -1,6 +0,0 @@
-import os, sys
-if len(sys.argv) == 1:
-    commit_message = "updates"
-else:
-    commit_message = " ".join(sys.argv[1:])
-os.system('git add . && git commit -m "{}" && git push origin main'.format(commit_message))
